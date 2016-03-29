66cf13449be8731c0be3697c250a703aaf257c3a
MikhniukR <r.mikhniuk@gmail.com>
2016-03-29 11:57:31 +0300

    Добавлен пустой repository

--- Lost/Lost/Models/FullUser.cs
+++ Lost/Lost/Models/FullUser.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MongoDB.Bson;
 
 namespace Lost.Models
 {
     //Для сохранения статистики о вещах
     public class FullUser:User
     {
-        public List<Guid> FoundThings { get; set; }
-        public List<Guid> LostThings { get; set; }  
+        public List<ObjectId> FoundThings { get; set; }
+        public List<ObjectId> LostThings { get; set; }  
     }
 }
