3842111364921355c5210fcd347b097bf3d93b4a
Andrey Shakel <shakel2000@mail.ru>
2016-03-29 21:43:47 +0300

    Поисправляли ошибки, но что-то все еще не так

--- Lost/Lost/Controllers/HomeController.cs
+++ Lost/Lost/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace Lost.Controllers
     public class ThingController : Controller
     {
         [HttpPost]
-        public ActionResult Index(string text0, string text1, string text2, string text3) // bool found, bool lost-)
+        public ActionResult Index(string text0, string text1, string text2, string text3) 
         {
             var place = text1;
             var about = text2;
@@ -19,11 +19,11 @@ namespace Lost.Controllers
             string total = "0";
             if (text0=="Lost")
             {
-                
+                total = text0 + text1 + text2 + text3;
             }
             else if(text0=="Found")
             {
-
+                total = text0 + text1 + text2 + text3;
             }
             return View(new IndexViewModels
             {
@@ -35,7 +35,6 @@ namespace Lost.Controllers
         [HttpGet]
         public ActionResult Index()
         {
-
             return View(new IndexViewModels
             {
                 text = ""
