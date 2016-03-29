282c30106b0d7a89e6b0b0464f56bd0ca638d731
MikhniukR <r.mikhniuk@gmail.com>
2016-03-29 16:23:14 +0300

    Закончил DbThingRepository

--- Lost/DataBase/DbRepository/DbThingRepository.cs
+++ Lost/DataBase/DbRepository/DbThingRepository.cs
@@ -1,33 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
-using System.Web;
 using Lost.Models;
 using Lost.Repositories;
-using MongoDB.Bson;
 using MongoDB.Driver;
+using MongoDB.Driver.Linq;
 
-namespace Lost.DbRepository
+namespace DataBase.DbRepository
 {
     public class DbThingRepository:IThingRepository
     {
-        private readonly IMongoCollection<Thing> _thingCollection;
+        private readonly IMongoCollection<Thing> _lostThingCollection;
+        private readonly IMongoCollection<Thing> _findThingCollection;
 
         public DbThingRepository()
         {
             var client = new MongoClient();
             var database = client.GetDatabase("mongodb");
-            _thingCollection = database.GetCollection<Thing>(nameof(Thing));
+            _lostThingCollection = database.GetCollection<Thing>("lostThings");
+            _findThingCollection = database.GetCollection<Thing>("findThings");
         }
         public void AddThing(Thing thing)
         {
-            _thingCollection.InsertOneAsync(thing);
+            _lostThingCollection.InsertOneAsync(thing);
         }
 
-        public List<Thing> FoundThing(string about)
+        public List<Thing> FindLostThing(string about)
         {
-            throw new NotImplementedException();
+            return _lostThingCollection.AsQueryable().Where(t => t.About.Equals(about)).ToList();
+        }
+
+        public List<Thing> FindFoundThing(string about)
+        {
+            return _findThingCollection.AsQueryable().Where(t => t.About.Equals(about)).ToList();
+
         }
     }
 }
