8748fa7d3162f9fb1d37aee368013db4272c209f
MikhniukR <r.mikhniuk@gmail.com>
2016-03-29 12:56:52 +0300

    Сделан IThingRepository

--- Lost/Lost/Repositories/IThingRepository.cs
+++ Lost/Lost/Repositories/IThingRepository.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Lost.Models;
+using MongoDB.Bson;
 
 namespace Lost.Repositories
 {
-    public class IThingRepository
+    public interface IThingRepository
     {
-       
+        void AddThing(Thing thing);
+        List<Thing> FoundThing(ObjectId id);
+        List<Thing> FoundThing(string about);
+
     }
 }
