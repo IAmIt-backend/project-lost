f1498f8865e8c7487ecd38069059eb254c363532
MikhniukR <r.mikhniuk@gmail.com>
2016-03-30 18:32:05 +0300

    Добавил UserRepository

---
+++ Lost/DB/Interfaces/IUserReposirory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DB.Models;
+using MongoDB.Bson;
+
+namespace DB.Interfaces
+{
+    public interface IUserReposirory
+    {
+        void AddUser(IdentityUser user);
+        IdentityUser GetUser(string id);
+    }
+}
