9c86fd2ba46bc71cfd8ceead9d84b8bbdcfda0eb
Andrey Shakel <shakel2000@mail.ru>
2016-03-30 20:09:37 +0300

    View updates

--- Lost/Lost/Controllers/HomeController.cs
+++ Lost/Lost/Controllers/HomeController.cs
@@ -28,38 +28,75 @@ namespace Lost.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(string status, string place, string about, string description)
+        public ActionResult ThingAdd(string status, string place, string about, string description, string submitButton, string returnButton)
         {
-            string total = "0";
-            Thing thing;            
-            if (status.Equals("lost"))
-            {
-                thing = new Thing
+            //  string total = "0";
+            
+           
+            //   return this.RedirectToAction("Info", "Home");
+            if (submitButton != null) {
+                Thing thing;
+                if (status.Equals("lost"))
                 {
-                    About = about,
-                    Description = description,
-                    Place = place,
-                    ItemStatus = ItemStates.Lost
-                };                
+                    thing = new Thing
+                    {
+                        About = about,
+                        Description = description,
+                        Place = place,
+                        ItemStatus = ItemStates.Lost
+                    };
+                }
+                else
+                {
+                    thing = new Thing
+                    {
+                        About = about,
+                        Description = description,
+                        Place = place,
+                        ItemStatus = ItemStates.Found
+                    };
+                }
+                //дальше будет это
+                //_logic.AddThing(thing);
+                return View(new IndexViewModel
+            {
+                Text = ""
+            });
             }
             else
             {
-                thing = new Thing
-                {
-                    About = about,
-                    Description = description,
-                    Place = place,
-                    ItemStatus = ItemStates.Found
-                };
+                return this.RedirectToAction("Index", "Home");
             }
-            _logic.AddThing(thing);
-            return View(new IndexViewModel
-            {
-                Text = total
-            });
-
         }
 
+        [HttpGet, Authorize]
+        public ActionResult ThingAdd()
+        {
+            return View(new IndexViewModel
+            {
+                Text = ""
+            });
+        }
+
+        [HttpPost]
+        public ActionResult Index(string search, string searchButton, string addButton)
+        {
+          
+            if(searchButton != null)
+            {
+        return View(new IndexViewModel
+            {
+                Text = search + " " + searchButton
+            });
+            }
+            else
+            {
+                return this.RedirectToAction("ThingAdd", "Home");
+            }
+           
+
+            
+        }
         [HttpGet]
         public ActionResult Index()
         {
@@ -68,5 +105,6 @@ namespace Lost.Controllers
                 Text = ""
             });
         }
+
     }
 }
