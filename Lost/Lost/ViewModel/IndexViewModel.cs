48d4333bc79feca9b15681cf4ed136be582a9a25
Andrey Shakel <shakel2000@mail.ru>
2016-03-31 15:08:53 +0300

    New views!!!!!!!!!!! ThingInfo почти готова

--- Lost/Lost/ViewModel/IndexViewModel.cs
+++ Lost/Lost/ViewModel/IndexViewModel.cs
@@ -2,12 +2,19 @@
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
 using DB.Models;
+using MongoDB.Bson;
 
 namespace Lost.ViewModel
 {
     public class IndexViewModel
     {
         public string Text { set; get; }
+        public string About { set; get; }
+        public string Description { set; get; }
+        public string Place { set; get; }
+        public ItemStates Status { set; get; }
+        public ObjectId UserId { set; get; }
+        public ObjectId ThingId { set; get; }
         public int Length { set; get; }
         public List<Thing> Things { set; get; }
     }
