5f5108b0805aa1e7c4ba52e851c47abeceef4d67
artembell <artembell305@gmail.com>
2016-03-31 14:32:42 +0300

    Полная asyncовая реализация

--- Lost/BusinessLogic/Interfaces/IBusinessLogic.cs
+++ Lost/BusinessLogic/Interfaces/IBusinessLogic.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using DB.Models;
 using MongoDB.Bson;
+using System.Threading.Tasks;
 
 namespace BusinessLogic.Interfaces
 {
@@ -8,10 +9,15 @@ namespace BusinessLogic.Interfaces
     {
 
 
-        Task<List<Thing>> FindThingASync(string about, ItemStates itemStates);
+        Task<List<Thing>> FindThingAsync(string about, ItemStates itemStates);
+        Task<List<Thing>> FindReturnedThingAsync(string about);
+        Task<Thing> AddThingAsync(Thing thing);
+        Task<Thing> DeleteThingAsync(ObjectId thingId);
+        Task<Thing> GetThingByIdAsync(ObjectId thingId);
+        List<Thing> FindThing(string about, ItemStates itemStates);
         List<Thing> FindReturnedThing(string about);
-        void AddThing(Thing thing);
-        void DeleteThing(ObjectId thingId);
+        Thing AddThing(Thing thing);
+        Thing DeleteThing(ObjectId thingId);
         Thing GetThingById(ObjectId thingId);
         //void authorize();
         //void login();
