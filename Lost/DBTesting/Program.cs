a3d8442212edc15ccaa56253d527e192782e1c68
MikhniukR <r.mikhniuk@gmail.com>
2016-04-01 12:00:37 +0300

    Более нормальное заполнение базы

--- Lost/DBTesting/Program.cs
+++ Lost/DBTesting/Program.cs
@@ -24,7 +24,9 @@ namespace DBTesting
             user.LastName = "Found";
             user.UserName = "Lost";
             user.Phone = "+123456789";
-            user = await _urepository.AddUserAsync(user);
+            //user = await _urepository.AddUserAsync(user);
+            user.UserId = new ObjectId("56fe349d0c60c92010174877");
+            user.Id = "56fe349d0c60c92010174877";
             Thing thing = new Thing();
             for (int i = 0; i < 10; i++)
             {
